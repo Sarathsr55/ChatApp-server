d9159a88e6282ca6bdf689440ef32e4724ea54a5
Sarathsr55 <95646297+Sarathsr55@users.noreply.github.com>
2025-01-30 17:10:25 +0530

    Update googleConfig.js

--- utils/googleConfig.js
+++ utils/googleConfig.js
@@ -1,7 +1,7 @@
 const {google} = require('googleapis')
 
-const GOOGLE_CLIENT_ID = ""
-const GOOGLE_CLIENT_SECRET = ""
+const GOOGLE_CLIENT_ID = PROCESS.ENV.GOOGLE_CLIENT_ID
+const GOOGLE_CLIENT_SECRET = PROCESS.ENV.GOOGLE_CLIENT_SECRET
 
 exports.oauth2client = new google.auth.OAuth2(
     GOOGLE_CLIENT_ID,
